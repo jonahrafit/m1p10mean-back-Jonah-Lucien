f67170e8d1f62bc211057de03f087de0701436b1
J-Lucien <jtolojanahary5@gmail.com>
2024-02-27 16:56:32 +0300

    fix rendezVous service

--- routes/rendezVousRouter.js
+++ routes/rendezVousRouter.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require( 'express' );
 const router = express.Router();
 
 const {
@@ -7,15 +7,16 @@ const {
     updateRendezVous,
     deleteRendezVous,
     getRendezVousByEmployeBetweenDate,
-    getRendezVousByEmployeID
-} = require('../service/RendezVousService');
+    getRendezVousByEmploye
+} = require( '../service/RendezVousService' );
 
 // SET AN APPOINTMENT FOR A CLIENT WITH HIS PREFER EMPLOYEE
-router.post('/:clientId/:serviceId/:employeeId', setRendezVousClientWithEmployee);
-router.get('/:page/:size', getRendezVous);
-router.put('/:id', updateRendezVous);
-router.delete('/:id', deleteRendezVous);
-router.get('/employee/:employeId/:debut/:fin', getRendezVousByEmployeBetweenDate);
-router.get('/employee/:employeId', getRendezVousByEmployeID);
+router.post( '/:clientId/:serviceId/:employeeId', setRendezVousClientWithEmployee );
+router.get( '/:employeId', getRendezVousByEmploye );
+router.get( '/:page/:size', getRendezVous );
+router.put( '/:id', updateRendezVous );
+router.delete( '/:id', deleteRendezVous );
+router.get( '/employee/:employeId/:debut/:fin', getRendezVousByEmployeBetweenDate );
+
 
 module.exports = router;
