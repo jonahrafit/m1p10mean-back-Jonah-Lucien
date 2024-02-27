fe8f97711f881670a3390bf6d6c92a1ff71b9c42
jonahrafit <jonahrafit.ram@hotmail.com>
2024-02-27 17:29:14 +0300

    fix express error for routing

--- routes/rendezVousRouter.js
+++ routes/rendezVousRouter.js
@@ -1,4 +1,4 @@
-const express = require( 'express' );
+const express = require('express');
 const router = express.Router();
 
 const {
@@ -8,14 +8,14 @@ const {
     deleteRendezVous,
     getRendezVousByEmployeBetweenDate,
     getRendezVousByEmployeID
-} = require( '../service/RendezVousService' );
+} = require('../service/RendezVousService');
 
 // SET AN APPOINTMENT FOR A CLIENT WITH HIS PREFER EMPLOYEE
 router.post('/:clientId/:serviceId/:employeeId', setRendezVousClientWithEmployee);
 router.put('/:id', updateRendezVous);
 router.delete('/:id', deleteRendezVous);
 router.get('/:page/:size', getRendezVous);
+router.get('/employee/:employeId/tous', getRendezVousByEmployeID); // Route générale après
 router.get('/employee/:employeId/:debut/:fin', getRendezVousByEmployeBetweenDate); // Route spécifique en premier
-router.get('/employee/:employeId', getRendezVousByEmployeID); // Route générale après
 
 module.exports = router;
--- service/RendezVousService.js
+++ service/RendezVousService.js
@@ -1,19 +1,19 @@
 const {
     RendezVous
-} = require( '../models/RendezVous' );
+} = require('../models/RendezVous');
 const {
     Client,
     clientSchema
-} = require( '../models/Client' );
+} = require('../models/Client');
 const {
     Employee
-} = require( '../models/Employe' );
+} = require('../models/Employe');
 const {
     Service
-} = require( '../models/Service' );
-const moment = require( 'moment' );
+} = require('../models/Service');
+const moment = require('moment');
 
-async function setRendezVousClientWithEmployee( req, res ) {
+async function setRendezVousClientWithEmployee(req, res) {
     try {
         const {
             clientId,
@@ -22,116 +22,116 @@ async function setRendezVousClientWithEmployee( req, res ) {
         } = req.params;
 
         // Vérifier le client
-        const clientById = await Client.findById( {
+        const clientById = await Client.findById({
             _id: clientId
-        } );
-        if ( !clientById ) throw new Error( 'Client not found' );
+        });
+        if (!clientById) throw new Error('Client not found');
 
         // Vérifier l'employé
-        const employeeById = await Employee.findById( {
+        const employeeById = await Employee.findById({
             _id: employeeId
-        } );
-        if ( !employeeById ) throw new Error( 'Employee not found' );
+        });
+        if (!employeeById) throw new Error('Employee not found');
 
         // Vérifier si l'employé est dans la liste de préférences du client
-        if ( !isEmployeeInClientPreferList( clientById, employeeById ) ) {
-            clientById.preferenceEmployees.push( {
+        if (!isEmployeeInClientPreferList(clientById, employeeById)) {
+            clientById.preferenceEmployees.push({
                 employee: employeeById,
                 niveauEtoile: 0
-            } );
+            });
             await clientById.save();
         } else {
-            console.log( "🚀 ~ setRendezVousClientWithEmployee ~ already in his list:" );
+            console.log("🚀 ~ setRendezVousClientWithEmployee ~ already in his list:");
         }
 
         // Vérifier le service
-        const serviceResult = await Service.findById( {
+        const serviceResult = await Service.findById({
             _id: serviceId
-        } );
-        if ( !serviceResult ) throw new Error( 'Service not found' );
+        });
+        if (!serviceResult) throw new Error('Service not found');
 
         // Vérifier si le service est dans la liste de préférences du client
-        if ( !isServiceInClientPreferList( clientById, serviceResult ) ) {
-            clientById.preferenceServices.push( {
+        if (!isServiceInClientPreferList(clientById, serviceResult)) {
+            clientById.preferenceServices.push({
                 service: serviceResult,
                 niveauEtoile: 0
-            } );
+            });
             await clientById.save();
         }
 
         // Sauvegarder un nouveau rendez-vous dans la base de données
-        const dateRendezVous = new Date( req.body.dateRendezVous );
-        const formattedDate = moment().utc( dateRendezVous, 'yyyy - mm - dd hh: mm: ss' );
-        console.log( "🚀 ~ setRendezVousClientWithEmployee ~ formattedDate:", formattedDate.isUTC() );
-        const rendezVous = new RendezVous( {
+        const dateRendezVous = new Date(req.body.dateRendezVous);
+        const formattedDate = moment().utc(dateRendezVous, 'yyyy - mm - dd hh: mm: ss');
+        console.log("🚀 ~ setRendezVousClientWithEmployee ~ formattedDate:", formattedDate.isUTC());
+        const rendezVous = new RendezVous({
             client: clientById,
             employee: employeeById,
             date_created: new Date(),
             date_rendez_vous: formattedDate,
             services: serviceResult,
             fait: false
-        } );
+        });
 
         // Sauvegarder le nouveau document RendezVous
         const rendezVousSaved = await rendezVous.save();
-        return res.status( 200 ).json( rendezVousSaved );
-    } catch ( error ) {
-        console.log( "🚀 ~ setRendezVousClientWithEmployee ~ error:", error );
-        return res.status( 400 ).json( {
+        return res.status(200).json(rendezVousSaved);
+    } catch (error) {
+        console.log("🚀 ~ setRendezVousClientWithEmployee ~ error:", error);
+        return res.status(400).json({
             error: error.message
-        } );
+        });
     }
 }
 
-async function getRendezVous( req, res ) {
+async function getRendezVous(req, res) {
     const {
         page,
         size
     } = req.params;
-    const skip = ( page - 1 ) * size;
-    console.log( "🚀 ~ getRendezVous ~ req.body:", req.body );
+    const skip = (page - 1) * size;
+    console.log("🚀 ~ getRendezVous ~ req.body:", req.body);
 
-    const rendezVous = await RendezVous.find( req.body ).skip( skip )
-        .limit( size );
-    if ( !rendezVous ) {
-        return res.status( 200 ).json( {
+    const rendezVous = await RendezVous.find(req.body).skip(skip)
+        .limit(size);
+    if (!rendezVous) {
+        return res.status(200).json({
             rendezVous: []
-        } );
+        });
     }
-    const date = moment( rendezVous.date_rendez_vous );
-    const formattedDate = date.format( 'YYYY-MM-DDTHH:mm:ss.SSSZ' ).replace( date.utcOffset().toString(), '+00:00' );
-    console.log( "🚀 ~ getRendezVous ~ formattedDate:", formattedDate );
-    return res.status( 200 ).json( {
+    const date = moment(rendezVous.date_rendez_vous);
+    const formattedDate = date.format('YYYY-MM-DDTHH:mm:ss.SSSZ').replace(date.utcOffset().toString(), '+00:00');
+    console.log("🚀 ~ getRendezVous ~ formattedDate:", formattedDate);
+    return res.status(200).json({
         rendezVous
-    } );
+    });
 
 }
 
-async function updateRendezVous( req, res ) {
+async function updateRendezVous(req, res) {
     const {
         id
     } = req.params;
-    const rendezVous = await RendezVous.findByIdAndUpdate( {
+    const rendezVous = await RendezVous.findByIdAndUpdate({
         _id: id
     }, req.body, {
         new: true
-    } );
-    if ( !rendezVous ) {
-        return res.status( 400 ).json( {
+    });
+    if (!rendezVous) {
+        return res.status(400).json({
             error: 'Rendez vous non trouve'
-        } );
+        });
     }
-    return res.status( 200 ).json( rendezVous );
+    return res.status(200).json(rendezVous);
 }
 
-async function deleteRendezVous( req, res ) {
-    const rendezVousDeleted = await RendezVous.findByIdAndDelete( {
+async function deleteRendezVous(req, res) {
+    const rendezVousDeleted = await RendezVous.findByIdAndDelete({
         _id: req.params.id
-    } );
-    return res.status( 200 ).json( rendezVousDeleted );
+    });
+    return res.status(200).json(rendezVousDeleted);
 }
 
-async function getRendezVousByEmployeBetweenDate( req, res ) {
+async function getRendezVousByEmployeBetweenDate(req, res) {
     try {
         const {
             employeId,
@@ -139,26 +139,26 @@ async function getRendezVousByEmployeBetweenDate( req, res ) {
             fin
         } = req.params;
 
-        const rendezVous = await RendezVous.find( {
+        const rendezVous = await RendezVous.find({
             'employee._id': employeId,
             date_rendez_vous: {
-                $gte: moment( debut ).startOf( 'day' ).toDate(),
-                $lte: moment( fin ).endOf( 'day' ).toDate()
+                $gte: moment(debut).startOf('day').toDate(),
+                $lte: moment(fin).endOf('day').toDate()
             }
-        } );
+        });
 
-        res.json( {
+        res.json({
             rendezVous
-        } );
-    } catch ( error ) {
-        console.error( error );
-        res.status( 500 ).json( {
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
             message: 'Une erreur est survenue lors de la récupération des rendez-vous.'
-        } );
+        });
     }
 }
 
-async function getRendezVousByEmploye( req, res ) {
+async function getRendezVousByEmployeID(req, res) {
     try {
         const {
             employeId
@@ -169,16 +169,16 @@ async function getRendezVousByEmploye( req, res ) {
             'employee._id': employeId
         };
 
-        const rendezVous = await RendezVous.find( query );
+        const rendezVous = await RendezVous.find(query);
 
-        return res.status( 200 ).json( {
+        return res.status(200).json({
             rendezVous
-        } );
-    } catch ( error ) {
-        console.error( error );
-        res.status( 500 ).json( {
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
             message: 'Une erreur est survenue lors de la récupération des rendez-vous.'
-        } );
+        });
     }
 }
 
@@ -191,9 +191,9 @@ function isEmployeeInClientPreferList(client, employee) {
     return false;
 }
 
-function isServiceInClientPreferList( client, service ) {
-    for ( const servPrefer of client.preferenceServices ) {
-        if ( servPrefer.service._id.equals( service._id ) ) {
+function isServiceInClientPreferList(client, service) {
+    for (const servPrefer of client.preferenceServices) {
+        if (servPrefer.service._id.equals(service._id)) {
             return true;
         }
     }
@@ -206,5 +206,5 @@ module.exports = {
     updateRendezVous,
     deleteRendezVous,
     getRendezVousByEmployeBetweenDate,
-    getRendezVousByEmploye
+    getRendezVousByEmployeID
 };
